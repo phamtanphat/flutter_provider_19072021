0bf4a1f6a0792c03229bed5ce538c9a798a15fb9
phamtanphat <phatdroid94@gmail.com>
2021-08-18 18:44:58 +0700

    provider

--- lib/demo_proxy_provider.dart
+++ lib/demo_proxy_provider.dart
@@ -1,20 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-class CounterApi{
+class CounterApi extends ChangeNotifier{
   int _count;
 
   CounterApi({required int count}) : this._count = count;
 
   int get count => _count;
+
+  set count(int count){
+    this._count = count;
+    notifyListeners();
+  }
 }
 
-class CounterService{
+class CounterService extends ChangeNotifier{
   late CounterApi _counterApi;
 
   CounterService();
 
   void updateCounterApi(CounterApi counterApi){
     this._counterApi = counterApi;
+    notifyListeners();
   }
 
   CounterApi get counterApi => _counterApi;
@@ -24,13 +30,14 @@ class CounterService{
   }
 }
 
-class CounterModel {
+class CounterModel extends ChangeNotifier{
   late CounterService _counterService;
 
   CounterModel();
 
   void updateCounterService(CounterService counterService){
     this._counterService = counterService;
+    notifyListeners();
   }
 
   CounterService get counterService => _counterService;
@@ -52,17 +59,19 @@ class _DemoProxyProviderState extends State<DemoProxyProvider> {
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
-        Provider(create: (context) => CounterApi(count: 10)),
-        ProxyProvider<CounterApi,CounterService>(
+        ChangeNotifierProvider(create: (context) => CounterApi(count: 10)),
+        ChangeNotifierProxyProvider<CounterApi,CounterService>(
           create: (context) => CounterService(),
           update: (context, counterApi , counterService){
+            print("update service");
             counterService!.updateCounterApi(counterApi);
             return counterService;
           },
         ),
-        ProxyProvider<CounterService,CounterModel>(
+        ChangeNotifierProxyProvider<CounterService,CounterModel>(
           create: (context) => CounterModel(),
           update: (context,counterService , counterModel){
+            print("update model");
             counterModel!.updateCounterService(counterService);
             return counterModel;
           },
@@ -89,8 +98,18 @@ class _ProxyContainerWidgetState extends State<ProxyContainerWidget> {
       body: Container(
         child: Center(
           child: Consumer<CounterModel>(
-            builder: (context , model , child){
-              return Text(model.counterService.counterApi.count.toString());
+            builder: (context , counterModel , child){
+              return Column(
+                children: [
+                  Text(counterModel.counterService.counterApi.count.toString()),
+                  ElevatedButton(
+                      onPressed: (){
+                        counterModel.counterService.counterApi.count++;
+                      },
+                      child: Text("Increment")
+                  )
+                ],
+              );
             },
           ),
         ),
